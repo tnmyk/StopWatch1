e7382bee9294986848e2171225e95f8502266f80
tanmayk5 <tanmaykachroo@gmail.com>
2021-03-27 03:42:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/stopwatch/Splash.kt
@@ -0,0 +1,23 @@
+package com.example.stopwatch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },1500)
+
+    }
+}
