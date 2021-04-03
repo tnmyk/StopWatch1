d080925ce470094ab86fed6e43e4c4746a591f02
tanmayk5 <73903781+tanmayk5@users.noreply.github.com>
2021-04-04 05:24:37 +0530

    Successfully added navigation but fragment gets destroyed when navigating

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,129 +1,35 @@
 package com.example.stopwatch
-
-import android.annotation.SuppressLint
-import android.content.Context
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock.elapsedRealtime
-import android.widget.Chronometer
-import android.widget.Toast
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
-import com.example.stopwatch.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
 
-    @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController =navHostFragment.findNavController()
 
-
-        var started = false
-        val meter = findViewById<Chronometer>(R.id.time)
-        var pausedtime: Long? = null
-        val mp = MediaPlayer.create(this, R.raw.zap)
-        mp.isLooping = false
-
-
-        // For Saving Theme
-        load_theme()
-        binding.themeswitch.setOnClickListener {
-            save_theme()
-            load_theme()
-        }
-
-        // Start , Stop , Reset Buttons
-        binding.startButton.setOnClickListener {
-
-            if (!started) {
-                binding.conctext.text = "#Winning"
-
-                if (pausedtime == null) {
-                    meter.base = elapsedRealtime()
-                } else {
-                    meter.base = elapsedRealtime() - pausedtime!!
-                }
-                binding.stopButton.isVisible = true
-
-                meter.start()
-                started = true
-                binding.resetButton.isVisible = false
-                binding.startButton.isVisible = false
-            }
-
-
-            mp.start()
-
-
-        }
-        binding.stopButton.setOnClickListener {
-            if (started) {
-                meter.stop()
-                pausedtime = elapsedRealtime() - meter.base
-
-                started = false
-                binding.conctext.text = "Paused"
-                binding.startButton.text = "Continue"
-                binding.stopButton.isInvisible = true
-                binding.resetButton.isVisible = true
-                binding.startButton.isVisible = true
-            }
-            mp.start()
-
-        }
-        binding.resetButton.setOnClickListener {
-            started = false
-            meter.stop()
-            pausedtime = null
-            binding.conctext.text = "Your Final Concentration Time:"
-            binding.startButton.text = "start again"
-            binding.stopButton.isInvisible = true
-            binding.resetButton.isVisible = false
-
-
-            mp.start()
-
-
-        }
+        bottom_nav.setupWithNavController(navController)
 
     }
 
-
-    private fun save_theme() {
-        val sharedPreferences = getSharedPreferences("themebtn", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.apply {
-            putBoolean("BOOLEAN_KEY", binding.themeswitch.isChecked)
-        }.apply()
-
-        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun load_theme() {
-        val sharedPreferences = getSharedPreferences("themebtn", Context.MODE_PRIVATE)
-        val savedBool = sharedPreferences.getBoolean("BOOLEAN_KEY", false)
-        binding.themeswitch.isChecked = savedBool
-
-        if (savedBool) {
-            binding.rl.setBackgroundResource(R.drawable.gradient_dark)
-            binding.time.setBackgroundResource(R.drawable.timecontainer_light)
-            binding.themeswitch.setBackgroundResource(R.drawable.lightcontainer)
-
-        } else {
-            binding.rl.setBackgroundResource(R.drawable.gradient_light)
-            binding.time.setBackgroundResource(R.drawable.timecontainer_dark)
-            binding.themeswitch.setBackgroundResource(R.drawable.darkcontainer)
-
-
-        }
-    }
 }
+
+
+
+
+
+
 
 
